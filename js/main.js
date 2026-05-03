300750ca0a96b3179f2e16605c962c3478a59190
njbohrer <njbohrer@gmail.com>
2026-05-03 12:10:14 -0400

    GeoJSON Load

--- js/main.js
+++ js/main.js
@@ -6,3 +6,24 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 L.control.scale().addTo(map);
+
+// Load breweries point GeoJSON
+fetch('data/Knoxville_Breweries_points.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data).addTo(map);
+    });
+
+// Load greenways line GeoJSON
+fetch('data/knoxville_greenways_lines.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data).addTo(map);
+    });
+
+// Load parks polygon GeoJSON
+fetch('data/knoxville_parks_polygons.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data).addTo(map);
+    });
