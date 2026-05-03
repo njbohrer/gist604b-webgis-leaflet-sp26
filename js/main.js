bd589be115414f9696ac1258e04dc4da1e255ed1
njbohrer <njbohrer@gmail.com>
2026-05-03 11:43:05 -0400

    repeated last step

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var map = L.map('map').setView([35.9606, -83.9207], 12);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; OpenStreetMap'
+}).addTo(map);
+
+L.control.scale().addTo(map);
